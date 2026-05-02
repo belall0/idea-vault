67302c12a800b8b0d3927b6bd23647c1ea372a7e
Belal Muhammad <belallmuhammad0@gmail.com>
2026-05-03 01:20:53 +0300

    feat: integrate tanstack query by configuring provider and router context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+const router = createRouter({ routeTree, context: { queryClient } });
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
@@ -12,7 +15,10 @@ declare module "@tanstack/react-router" {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+    ,
+  </QueryClientProvider>,
 );
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,16 @@
-import { HeadContent, createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  HeadContent,
+  createRootRouteWithContext,
+  Outlet,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { QueryClient } from "@tanstack/react-query";
 
-export const Route = createRootRoute({
+type RouterContext = {
+  queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RouterContext>()({
   head: () => ({
     meta: [
       {
