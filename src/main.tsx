9fd98349b80415ce84ee33cfb20172c3b29ed72a
Belal Muhammad <belallmuhammad0@gmail.com>
2026-05-03 01:41:23 +0300

    feat: implement TanStack Query with suspense and add devtools for idea details page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
@@ -18,7 +19,7 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </StrictMode>
-    ,
   </QueryClientProvider>,
 );
--- src/routes/ideas/$ideaid/index.tsx
+++ src/routes/ideas/$ideaid/index.tsx
@@ -1,21 +1,41 @@
-import { createFileRoute } from "@tanstack/react-router";
+// TODO: learn about react suspense and error boundaries
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import type { Idea } from "../../../types";
 
-const fetchIdea = async (ideaId: string) => {
+const fetchIdea = async (ideaId: string): Promise<Idea> => {
   const res = await fetch(`/api/ideas/${ideaId}`);
   if (!res.ok) throw new Error("Failed to fetch Data");
   const idea = await res.json();
   return idea;
 };
 
+const ideaQueryOptions = (ideaId: string) => {
+  return queryOptions({
+    queryKey: ["ideas", ideaId],
+    queryFn: async () => fetchIdea(ideaId),
+  });
+};
+
 export const Route = createFileRoute("/ideas/$ideaid/")({
   component: IdeaDetailsPage,
-  loader: async ({ params }) => {
-    return fetchIdea(params.ideaid);
+  loader: async ({ params, context: { queryClient } }) => {
+    return queryClient.ensureQueryData(ideaQueryOptions(params.ideaid));
   },
 });
 
 function IdeaDetailsPage() {
-  const idea = Route.useLoaderData();
+  const { data: idea } = useSuspenseQuery(
+    ideaQueryOptions(Route.useParams().ideaid),
+  );
 
-  return <div>{idea.title}</div>;
+  return (
+    <div className="p-4">
+      <Link to="/ideas" className="black m-4 text-blue-500 underline">
+        Back to Ideas
+      </Link>
+      <h2 className="text-2xl font-bold">{idea.title}</h2>
+      <p className="mt-2">{idea.description}</p>
+    </div>
+  );
 }
