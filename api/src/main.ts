f89565154afc2d84c267508dba1292d7b66d5c42
Belal Muhammad <belallmuhammad0@gmail.com>
2026-05-24 09:01:27 +0300

    feat: add helmet for enhanced security in API

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
